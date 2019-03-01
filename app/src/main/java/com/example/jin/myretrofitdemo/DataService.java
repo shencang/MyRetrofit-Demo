23ad623dec29b9389476b558affc929d0b070939
shencang <2630610009@qq.com>
2019-03-01 23:01:24 +0800

    初次提交

---
+++ app/src/main/java/com/example/jin/myretrofitdemo/DataService.java
@@ -0,0 +1,12 @@
+package com.example.jin.myretrofitdemo;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+public interface DataService {
+    @GET
+    Call<String>baidu(@Url String url);
+
+}
+
