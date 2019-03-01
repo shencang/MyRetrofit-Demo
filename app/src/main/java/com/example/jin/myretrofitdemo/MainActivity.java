d060aaa353ad4c5a7e34de0b14e0abe4bd2b6d7b
shencang <2630610009@qq.com>
2019-03-01 23:29:28 +0800

    简介和资料，精简空白行

--- app/src/main/java/com/example/jin/myretrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/jin/myretrofitdemo/MainActivity.java
@@ -17,53 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
         //创建Retrofit对象
-
         Retrofit retrofit = new Retrofit.Builder()
-
                 //指定baseurl，这里有坑，最后后缀出带着“/”
-
                 .baseUrl("http://www.baidu.com/")
-
                 //设置内容格式,这种对应的数据返回值是String类型
-
                 .addConverterFactory(ScalarsConverterFactory.create())
-
                 //定义client类型
-
                 .client(new OkHttpClient())
-
                 //创建
-
                 .build();
 
         //通过retrofit和定义的有网络访问方法的接口关联
-
         DataService dataService = retrofit.create(DataService.class);
-
         //在这里又重新设定了一下baidu的地址，是因为Retrofit要求传入具体，如果是决定路径的话，路径会将baseUrl覆盖掉
-
-        Call<String> baidu = dataService.baidu("http://wwww.baidu.com");
-
+        Call<String> baidu = dataService.baidu("http://www.baidu.com");
         //执行异步请求
-
         baidu.enqueue(new Callback<String>() {
-
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-
                 Toast.makeText(MainActivity.this,  response.body(), Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
             public void onFailure(Call<String> call, Throwable t) {
-
-
 
             }
 
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # 学习Retrofit的简单Android App
+     之后会在这里更新学习Retrofit的一系列代码，最终目标是掌握Retrofit的用法，并实现
+     https://www.jianshu.com/p/3487b8874b71的内容。
+
+### 参考资料：
+     Retrofit2深入浅出：https://www.jianshu.com/p/f2644cc784f3
+     网络加载框架 - Retrofit：https://www.jianshu.com/p/0fda3132cf98
+     Retrofit 最简单的快速入门及封装：https://blog.csdn.net/chenjie0932/article/details/79558050
+### Retrofit官方网站：
+     https://square.github.io/retrofit/
